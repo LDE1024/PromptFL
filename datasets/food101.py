e2fc6eec57bfb7d1958fc8cc375b0ba77398eee2
Li <78217848+LDE1024@users.noreply.github.com>
2026-03-10 12:18:08 +0800

    Update food101.py

--- datasets/food101.py
+++ datasets/food101.py
@@ -1,23 +1,28 @@
 import os
 import pickle
-
+import random
+from scipy.io import loadmat
+from collections import defaultdict
+import random
+from collections import defaultdict
 from dassl.data.datasets import DATASET_REGISTRY, Datum, DatasetBase
-from dassl.utils import mkdir_if_missing
+from dassl.utils import read_json, mkdir_if_missing
 
 from .oxford_pets import OxfordPets
-from .dtd import DescribableTextures as DTD
 
 
 @DATASET_REGISTRY.register()
-class Food101(DatasetBase):
+class OxfordFlowers(DatasetBase):
 
-    dataset_dir = "food-101"
+    dataset_dir = "oxford_flowers"
 
     def __init__(self, cfg):
         root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
         self.dataset_dir = os.path.join(root, self.dataset_dir)
-        self.image_dir = os.path.join(self.dataset_dir, "images")
-        self.split_path = os.path.join(self.dataset_dir, "split_zhou_Food101.json")
+        self.image_dir = os.path.join(self.dataset_dir, "jpg")
+        self.label_file = os.path.join(self.dataset_dir, "imagelabels.mat")
+        self.lab2cname_file = os.path.join(self.dataset_dir, "cat_to_name.json")
+        self.split_path = os.path.join(self.dataset_dir, "split_zhou_OxfordFlowers.json")
         self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
         self.baseline_dir = os.path.join(self.dataset_dir, "baseline")
         mkdir_if_missing(self.split_fewshot_dir)
@@ -25,7 +30,7 @@ class Food101(DatasetBase):
         if os.path.exists(self.split_path):
             total_train, val, test = OxfordPets.read_split(self.split_path, self.image_dir)
         else:
-            total_train, val, test = DTD.read_and_split_data(self.image_dir)
+            total_train, val, test = self.read_data()
             OxfordPets.save_split(total_train, val, test, self.split_path, self.image_dir)
 
         num_shots = cfg.DATASET.NUM_SHOTS
@@ -49,7 +54,8 @@ class Food101(DatasetBase):
                 print(f"Saving preprocessed few-shot data to {preprocessed}")
                 # with open(preprocessed, "wb") as file:
                 #     pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
-
+        if num_shots < 1:
+            train = total_train
         subsample = cfg.DATASET.SUBSAMPLE_CLASSES
         train, val, test = OxfordPets.subsample_classes(train, val, test, subsample=subsample)
 
@@ -63,4 +69,68 @@ class Food101(DatasetBase):
         else:
             federated_train_x = None
 
-        super().__init__(train_x=train, federated_train_x=federated_train_x, val=val, test=test)
+
+        # 终极保护：防止底层 Dassl 不认识 federated_train_x 参数导致崩溃
+        try:
+            super().__init__(train_x=train, federated_train_x=federated_train_x, val=val, test=test)
+        except TypeError:
+            self.federated_train_x = federated_train_x
+            super().__init__(train_x=train, val=val, test=test)
+
+    def read_data(self):
+        tracker = defaultdict(list)
+        label_file = loadmat(self.label_file)["labels"][0]
+        for i, label in enumerate(label_file):
+            imname = f"image_{str(i + 1).zfill(5)}.jpg"
+            impath = os.path.join(self.image_dir, imname)
+            label = int(label)
+            tracker[label].append(impath)
+
+        print("Splitting data into 50% train, 20% val, and 30% test")
+
+        def _collate(ims, y, c):
+            items = []
+            for im in ims:
+                item = Datum(impath=im, label=y - 1, classname=c)  # convert to 0-based label
+                items.append(item)
+            return items
+
+        lab2cname = read_json(self.lab2cname_file)
+        train, val, test = [], [], []
+        for label, impaths in tracker.items():
+            random.shuffle(impaths)
+            n_total = len(impaths)
+            n_train = round(n_total * 0.5)
+            n_val = round(n_total * 0.2)
+            n_test = n_total - n_train - n_val
+            assert n_train > 0 and n_val > 0 and n_test > 0
+            cname = lab2cname[str(label)]
+            train.extend(_collate(impaths[:n_train], label, cname))
+            val.extend(_collate(impaths[n_train : n_train + n_val], label, cname))
+            test.extend(_collate(impaths[n_train + n_val :], label, cname))
+
+        return train, val, test
+    def generate_federated_dataset(self, dataset, num_shots=-1, num_users=5, is_iid=True, repeat_rate=1.0):
+        dict_users = {}
+        dataset_list = list(dataset)
+        random.shuffle(dataset_list)
+        chunk_size = len(dataset_list) // num_users
+        for i in range(num_users):
+            dict_users[i] = dataset_list[i*chunk_size : (i+1)*chunk_size]
+        return dict_users
+
+    def generate_federated_fewshot_dataset(self, dataset, num_shots=-1, num_users=5, is_iid=True, repeat_rate=1.0):
+        dict_users = {}
+        tracker = defaultdict(list)
+        for item in dataset:
+            tracker[item.label].append(item)
+            
+        for i in range(num_users):
+            user_data = []
+            for label, items in tracker.items():
+                if len(items) >= num_shots and num_shots > 0:
+                    user_data.extend(random.sample(items, num_shots))
+                else:
+                    user_data.extend(items)
+            dict_users[i] = user_data
+        return dict_users
